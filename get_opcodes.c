f367a6725da2e91eca9b9ed36f6b00a053e91480
ChristianMpai <tebogotebzana@gmail.com>
2022-09-08 19:40:09 -0700

    rotr opcode.

--- get_opcodes.c
+++ get_opcodes.c
@@ -14,6 +14,20 @@ void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number)
 		{"pall", _pall},
 		{"pint", _pint},
 		{"pop", _pop},
+		{"swap", _swap},
+		{"queue", _queue},
+		{"stack", _stack},
+		{"add", _add},
+		{"nop", _nop},
+		{"sub", _sub},
+		{"mul", _mul},
+		{"div", _div},
+		{"mod", _mod},
+		{"pchar", _pchar},
+		{"pstr", _pstr},
+		{"rotl", _rotl},
+		{"rotr", _rotr},
+		{NULL, NULL}
 
 	};
 	int i;
